a3f74fb32501bd185ae7049f63806a2e0bec9b91
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-08-27 13:58:36 +0200

    Update birkhoff.jl; Add test

--- examples/birkhoff.jl
+++ examples/birkhoff.jl
@@ -93,3 +93,52 @@ end
     @show f(x), f(x_mip)
     @test isapprox(f(x_mip), f(x), atol=1e-6, rtol=1e-2)
 end 
+
+# test with mixed integer constraint
+#=@testset "Birkhoff polytope mixed interger" begin
+    n = 4
+    d = randn(n, n)
+    int_vars = collect(n^2/2:n^2)
+    int_num = Int(n^2 / 2 + 1)
+    lower_bounds = fill(0.0, int_num)
+    upper_bounds = fill(1.0, int_num)
+    sblmo = BirkhoffBLMO(true, n, int_vars)
+    lmo = build_birkhoff_mip(n; lower_bounds=lower_bounds, upper_bounds=upper_bounds, int_vars=int_vars)
+    x = ones(n, n) ./ n
+    # test without fixings
+    v_if = Boscia.bounded_compute_inface_extreme_point(sblmo, d, x, lower_bounds, upper_bounds, int_vars)
+    v_fw = Boscia.bounded_compute_extreme_point(sblmo, d, lower_bounds, upper_bounds, int_vars)
+    v_fw_MOI = Boscia.compute_extreme_point(lmo, d)
+    v_fw_MOI = vec(v_fw_MOI)
+    @test norm(v_fw - v_if) ≤ n * eps()
+    @test norm(v_if - v_fw_MOI) ≤ n * eps()
+    fixed_col = 2
+    fixed_row = 3
+    # fix one transition and renormalize
+    x2 = copy(x)
+    x2[:, fixed_col] .= 0
+    x2[fixed_row, :] .= 0
+    x2[fixed_row, fixed_col] = 1
+    x2 = x2 ./ sum(x2, dims=1)
+    v_fixed = Boscia.bounded_compute_inface_extreme_point(sblmo, d, x2, lower_bounds, upper_bounds, int_vars)
+    idx = (fixed_col-1)*n+fixed_row
+    @test v_fixed[idx] == 1
+    # If matrix is already a vertex, away-step can give only itself
+    @test norm(Boscia.bounded_compute_inface_extreme_point(sblmo, d, v_fixed, lower_bounds, upper_bounds, int_vars) - v_fixed) ≤ eps()
+    # fixed a zero only
+    x3 = copy(x)
+    x3[4, 3] = 0
+    # fixing zeros by creating a cycle 4->3->1->4->4
+    x3[4, 4] += 1 / n
+    x3[1, 4] -= 1 / n
+    x3[1, 3] += 1 / n
+    v_zero = Boscia.bounded_compute_inface_extreme_point(sblmo, d, x3, lower_bounds, upper_bounds, int_vars)
+    idx = (3-1)*n+4
+    @test v_zero[idx] == 0
+    idx = (4-1)*n+1
+    @test v_zero[idx] == 0
+    # test with fixed bounds
+    lower_bounds[Int(n^2/2)] = 1.0
+    sblmo = BirkhoffBLMO(true, n, int_vars)
+    lmo = build_birkhoff_mip(n; lower_bounds=lower_bounds, upper_bounds=upper_bounds, int_vars=int_vars)
+end=#
